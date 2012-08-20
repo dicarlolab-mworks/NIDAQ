518ce700c6b53cce4f4ca92e8e7635ce7fceebef
Christopher Stawarz <cstawarz@mit.edu>
2012-08-20 13:36:50 -0400

    Initial Commit

---
+++ nidaqtest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  nidaqtest
+//
+//  Created by Christopher Stawarz on 8/20/12.
+//  Copyright (c) 2012 Christopher Stawarz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
