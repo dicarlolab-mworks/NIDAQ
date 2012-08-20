2de06bb6b681e50ef1047a47b07226beaf9598a2
Christopher Stawarz <cstawarz@mit.edu>
2012-08-20 14:16:02 -0400

    Display device serial number.  Changed project organization name to MIT.

--- nidaqtest/main.cpp
+++ nidaqtest/main.cpp
@@ -3,16 +3,31 @@
 //  nidaqtest
 //
 //  Created by Christopher Stawarz on 8/20/12.
-//  Copyright (c) 2012 Christopher Stawarz. All rights reserved.
+//  Copyright (c) 2012 MIT. All rights reserved.
 //
 
 #include <iostream>
+#include <string>
+
+#include <NIDAQmxBase.h>
+
 
 int main(int argc, const char * argv[])
 {
+    const char *device = "Dev1";
+    
+    std::cout << "Fetching serial number of device " << device << std::endl;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    uInt32 serialNumber;
+    int32 error = DAQmxBaseGetDevSerialNum(device, &serialNumber);
+    
+    if (error == 0) {
+        std::cout << "Serial number = " << std::hex << std::uppercase << serialNumber;
+    } else {
+        std::cout << "error = " << error;
+    }
+    std::cout << std::endl;
+    
     return 0;
 }
 
