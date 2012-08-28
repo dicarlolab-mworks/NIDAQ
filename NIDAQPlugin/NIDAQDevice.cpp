bb424385e012705ebabfb62c2e0820dbdab7c088
Christopher Stawarz <cstawarz@mit.edu>
2012-08-28 14:34:27 -0400

    Try spawning helper process in NIDAQDevice constructor.  Install helper executable outside of plugin executables directory, so that it doesn't appear in the Dock when running.

--- NIDAQPlugin/NIDAQDevice.cpp
+++ NIDAQPlugin/NIDAQDevice.cpp
@@ -8,6 +8,9 @@
 
 #include "NIDAQDevice.h"
 
+#include <spawn.h>
+#include <sys/wait.h>
+
 
 BEGIN_NAMESPACE_MW
 
@@ -26,7 +29,51 @@ void NIDAQDevice::describeComponent(ComponentInfo &info) {
 
 NIDAQDevice::NIDAQDevice(const ParameterValueMap &parameters) :
     IODevice(parameters)
-{ }
+{
+    pid_t pid;
+    const char * const argv[2] = { PLUGIN_HELPER_EXECUTABLE, 0 };
+    int status = posix_spawn(&pid,
+                             PLUGIN_HELPERS_DIR "/" PLUGIN_HELPER_EXECUTABLE,
+                             NULL,
+                             NULL,
+                             const_cast<char * const *>(argv),
+                             NULL);
+    
+    if (0 == status) {
+        mprintf(M_IODEVICE_MESSAGE_DOMAIN, "Successfully launched helper process");
+        int stat;
+        waitpid(pid, &stat, 0);
+    } else {
+        merror(M_IODEVICE_MESSAGE_DOMAIN, "Helper process failed to launch (status = %d)", status);
+    }
+}
 
 
 END_NAMESPACE_MW
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
